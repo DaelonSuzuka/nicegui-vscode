ee6d6d54092bbaf3b678050b2bffb181e920ec65
David Kincaid <daelonsuzuka@gmail.com>
2024-10-06 14:50:20 -0400

    Syntax highlighting is working for html, css, scss, and sass literals

--- syntaxes/examples/example.py
+++ syntaxes/examples/example.py
@@ -1,21 +1,69 @@
 from nicegui import ui
 
-tree = ui.tree([
-    {'id': 'numbers', 'description': 'Just some numbers', 'children': [
-        {'id': '1', 'description': 'The first number'},
-        {'id': '2', 'description': 'The second number'},
-    ]},
-    {'id': 'letters', 'description': 'Some latin letters', 'children': [
-        {'id': 'A', 'description': 'The first letter'},
-        {'id': 'B', 'description': 'The second letter'},
-    ]},
-], label_key='id', on_select=lambda e: ui.notify(e.value))
+a = f''
 
-tree.add_slot('default-header', '''
-    <span :props="props">Node <strong>{{ props.node.id }}</strong></span>
-''')
-tree.add_slot('default-body', '''
+
+ui.add_head_html("<style>.my-label {background: rgb(0, 0, 255)}</style>")
+ui.add_body_html(
+    """
+<div>
+    <span>
+    text or something, idk
+    </span>
+</div>
+""",
+    shared=True,
+)
+
+ui.add_css(r"""
+.red {
+    color: red;
+}
+""")
+ui.add_scss(
+    """
+    .green {
+        background-color: lightgreen;
+        .blue {
+            color: blue;
+        }
+    }
+    """,
+    indented=True,
+)
+ui.add_sass(
+    """
+    .yellow
+        background-color: yellow
+        .purple
+            color: purple
+"""
+)
+
+tree = ui.tree()
+
+tree.add_slot(
+    "default-header",
+    '<span :props="props">Node <strong>{{ props.node.id }}</strong></span>',
+)
+tree.add_slot(
+    "default-header",
+    '<span :props="props">Node <strong>{{ props.node.id }}</strong></span>',
+)
+tree.add_slot(
+    "default-body",
+    """
     <span :props="props">Description: "{{ props.node.description }}"</span>
-''')
+""",
+)
+tree.add_slot(
+    "default-body",
+    """
+    <span :props="props">Description: "{{ props.node.description }}"</span>
+""",
+)
+
+with ui.row().classes("w-full").style("background-color=red;"):
+    pass
 
 ui.run()
