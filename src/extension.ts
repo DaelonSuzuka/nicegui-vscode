9fd451efc3dcfa4e297f0f174fd6c0bf13c14e33
David Kincaid <daelonsuzuka@gmail.com>
2024-10-23 08:42:01 -0400

    Have extension automatically enable quick suggestions in strings

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,27 @@
 import type * as vscode from "vscode";
-import {
-	NiceGuiCompletionItemProvider
-} from "./providers";
+import { NiceGuiCompletionItemProvider } from "./providers";
+import { createLogger, get_config } from "./utils";
 
+const log = createLogger("ext");
 
 export function activate(context: vscode.ExtensionContext) {
-    new NiceGuiCompletionItemProvider(context);
+	new NiceGuiCompletionItemProvider(context);
+
+	if (get_config().get("enableStringSuggestions")) {
+		enable_string_suggestions();
+	}
+}
+
+function enable_string_suggestions() {
+	const setting = "editor.quickSuggestions";
+	const scope = { languageId: "python" };
+
+	const config = get_config("", scope);
+	const currentValue = config.get(setting, {});
+	const value = { ...currentValue, ...{ strings: "on" } };
+	config.update(setting, value);
 }
 
 export function deactivate() {
-
+	//
 }
