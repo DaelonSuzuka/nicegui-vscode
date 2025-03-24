cdc0fb6e614ad33ec346677e15d0e9621d03fa26
David Kincaid <daelonsuzuka@gmail.com>
2025-03-24 13:37:47 -0400

    Add alt+o shortcut to switch from a python file to a related vue component

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
-import type * as vscode from 'vscode';
+import * as vscode from 'vscode';
 import { NiceGuiCompletionItemProvider, NiceGuiHoverProvider } from './providers';
-import { createLogger, get_config } from './utils';
+import { createLogger, find_file, get_config, register_command, set_context } from './utils';
 
 const log = createLogger('ext');
 
 export function activate(context: vscode.ExtensionContext) {
 	new NiceGuiCompletionItemProvider(context);
 	new NiceGuiHoverProvider(context);
+
+	context.subscriptions.push(register_command('switchScriptComponent', switch_script_component));
+
+	set_context('niceguiComponentFiles', ['python', 'vue', 'javascript']);
 
 	if (get_config().get('enableStringSuggestions')) {
 		enable_string_suggestions();
@@ -25,4 +29,22 @@ function enable_string_suggestions() {
 
 export function deactivate() {
 	//
+}
+
+async function switch_script_component() {
+	const currentFile = vscode.window.activeTextEditor.document.uri.fsPath;
+	let targetFile = '';
+	if (currentFile.endsWith('.py')) {
+		// TODO: also check for JS files somehow
+		targetFile = currentFile.replace('.py', '.vue');
+	} else if (currentFile.endsWith('.vue')) {
+		targetFile = currentFile.replace('.vue', '.py');
+	} else if (currentFile.endsWith('.js')) {
+		targetFile = currentFile.replace('.js', '.py');
+	}
+
+	const file = await find_file(targetFile);
+	if (file) {
+		vscode.window.showTextDocument(file);
+	}
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import * as vscode from 'vscode';
 
 export * from './logger';
@@ -10,4 +12,27 @@ export function get_config(section = EXTENSION_PREFIX, scope?: vscode.Configurat
 
 export function is_debug_mode(): boolean {
 	return process.env.VSCODE_DEBUG_MODE === 'true';
+}
+
+const CONTEXT_PREFIX = `${EXTENSION_PREFIX}.context.`;
+
+export function set_context(name: string, value: any) {
+	return vscode.commands.executeCommand('setContext', CONTEXT_PREFIX + name, value);
+}
+
+export function register_command(command: string, callback: (...args: any[]) => any, thisArg?: any) {
+	return vscode.commands.registerCommand(`${EXTENSION_PREFIX}.${command}`, callback, thisArg);
+}
+
+export async function find_file(file: string): Promise<vscode.Uri | null> {
+	if (fs.existsSync(file)) {
+		return vscode.Uri.file(file);
+	}
+	const fileName = path.basename(file);
+	const results = await vscode.workspace.findFiles(`**/${fileName}`, null);
+	if (results.length === 1) {
+		return results[0];
+	}
+
+	return null;
 }
